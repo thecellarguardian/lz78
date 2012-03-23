b4d67e7d626aa9cea464576c69657f510766d4c0
cosimosacco@gmail.com <cosimosacco@gmail.com@c677ba40-6e36-5728-7c26-4defcc0a1129>
2012-03-23 17:53:07 +0000

    Headers

--- trunk/lib/BitwiseBufferedFile/bufferConfiguration.h
+++ trunk/lib/BitwiseBufferedFile/bufferConfiguration.h
@@ -1,4 +1,23 @@
-
+/**
+ * @file bufferConfiguration.h
+ * @author Cosimo Sacco <cosimosacco@gmail.com>
+ * @author Davide Silvestri <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 /**
  * CELL_TYPE_LENGTH is a configurable paramether.
  * A definition must be given before including configurableBuffer.h.
