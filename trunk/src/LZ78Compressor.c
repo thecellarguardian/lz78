359a49e467bef3b8cc0d458907b8fb665ef2da88
cosimosacco@gmail.com <cosimosacco@gmail.com@c677ba40-6e36-5728-7c26-4defcc0a1129>
2012-03-26 15:58:19 +0000

    Caronte

--- trunk/src/LZ78Compressor.c
+++ trunk/src/LZ78Compressor.c
@@ -25,32 +25,32 @@
 
 int compress(const char* inputFile, const char* outputFile)
 {
-    CELL_TYPE child_index = ROOT_INDEX + 1;
-    CELL_TYPE lookup_index = ROOT_INDEX;
+    CELL_TYPE childIndex = ROOT_INDEX + 1;
+    CELL_TYPE lookupIndex = ROOT_INDEX;
     CELL_TYPE indexLengthMask = INDEX_LENGTH_MASK;
     CELL_TYPE child;
     struct BitwiseBufferedFile* r = openBitwiseBufferedFile(inputFile, 0);
     struct BitwiseBufferedFile* w = openBitwiseBufferedFile(outputFile, 1);//ATTENZIONE: e se fossero socket? generalizzare a descrittore di file
     size_t indexLength = INITIAL_INDEX_LENGTH;
     uint8_t read;
-    ssize_t bitsRead = 0;
+    ssize_t readBits = 0;
     LZ78HashTable* hash_table;
     if(r == NULL || w == NULL)
     {
+        errno = EINVAL;
         return -1;
     }
     hash_table = hash_initialize();//TODO inizializzazione hash_table
     while(r->emptyFile == 0)
     {
-        bitsRead += readBitBuffer(r, &read, 8);//TODO gestione errori + byte per byte
-        child = hash_lookup(hash_table, lookup_index, read); //TODO
-        if(child != -1){
-            lookup_index = child;
-        }
-        else{ //read not found in table
-            writeBitBuffer(w, lookup_index, indexLength); //TODO gestione errori
-            hash_insert(hash_table, lookup_index, read, child_index);//TODO funzione
-            child_index++;
+        readBits += readBitBuffer(r, &read, 8);//TODO gestione errori + byte per byte
+        child = hash_lookup(hash_table, lookupIndex, read); //TODO
+        if(child != -1) lookupIndex = child;
+        else
+        { //read not found in table
+            writeBitBuffer(w, lookupIndex, indexLength); //TODO gestione errori
+            hash_insert(hash_table, lookupIndex, read, childIndex);//TODO funzione
+            childIndex++;
             /**
              * If the number of children reaches the next power of 2, the
              * related index length must be incremented by one.
@@ -58,25 +58,25 @@ int compress(const char* inputFile, const char* outputFile)
              * the index transmitted to the decompressor is log_2(k) long, no
              * more.
              **/
-            if(child_index & indexLengthMask == 0) //if the next power of 2 is reached...
+            if(childIndex & indexLengthMask == 0) //if the next power of 2 is reached...
             {
                 indexLength++; //...the length of the transmitted index is incremented...
                 indexLengthMask = (indexLengthMask << 1) | 1; //...and the next power of 2 to check is set
             }
-            //lookup_index = ROOT_INDEX;
-            lookup_index = read; //ascii code of read is read's index. next lookup starts from read.
-            if (child_index == MAX_CHILD){
+            //lookupIndex = ROOT_INDEX;
+            lookupIndex = read; //ascii code of read is read's index. next lookup starts from read.
+            if (childIndex == MAX_CHILD)
+            {
               hash_reset(hash_table);//TODO
-              child_index = ROOT_INDEX + 1;
+              childIndex = ROOT_INDEX + 1;
             }
         }
     }
-    writeBitBuffer(w, lookup_index, indexLength); //TODO gestione errori + #! decompressor aaa radius
-    /*if(lookup_index != ROOT_INDEX){ //se non era il fine file ma l'ultimo simbolo non riconosciuto
+    writeBitBuffer(w, lookupIndex, indexLength); //TODO gestione errori + #! decompressor aaa radius
+    /*if(lookupIndex != ROOT_INDEX){ //se non era il fine file ma l'ultimo simbolo non riconosciuto
        writeBitBuffer(w, ROOT_INDEX, INDEX_LENGTH);
     }*/
     writeBitBuffer(w, ROOT_INDEX, INITIAL_INDEX_LENGTH); //Fine file
     closeBitwiseBufferedFile(r);
-
-    return bitsRead;
+    return readBits;
 }
