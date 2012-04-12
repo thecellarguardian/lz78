b9005c745d16b6e2633de1fbf830b066eb8fa34e
cosimosacco@gmail.com <cosimosacco@gmail.com@c677ba40-6e36-5728-7c26-4defcc0a1129>
2012-04-12 18:52:43 +0000

    O

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -5,12 +5,15 @@ int main(){
     FILE* f1 = fopen("rfc4227.txt","r");
    // FILE* f1 = fopen("prova.pdf","r");
     FILE* f2 = fopen("prova2","w+");
-    FILE* f3 = fopen("prova3","w+");
-    int result = 0;
-    result = compress(f1, f2);
-    printf("\n%i\n", result);
+    FILE* f3;
+    compress(f1, f2);
+    fclose(f1);
     fclose(f2);
+    /*
     f2 = fopen("prova2","r");
-    printf("\n%i\n",decompress(f2,f3));
+    f3 = fopen("prova3","w+");
+    decompress(f2,f3);
+    fclose(f2);
+    fclose(f3);*/
     return 0;
 }
