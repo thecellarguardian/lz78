8fd2809643829fb9a8af77adf8485f3f35f34b90
cosimosacco@gmail.com <cosimosacco@gmail.com@c677ba40-6e36-5728-7c26-4defcc0a1129>
2012-04-12 18:29:23 +0000

    OK!

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -6,7 +6,9 @@ int main(){
    // FILE* f1 = fopen("prova.pdf","r");
     FILE* f2 = fopen("prova2","w+");
     FILE* f3 = fopen("prova3","w+");
-    printf("\n%i\n",compress(f1,f2));
+    int result = 0;
+    result = compress(f1, f2);
+    printf("\n%i\n", result);
     fclose(f2);
     f2 = fopen("prova2","r");
     printf("\n%i\n",decompress(f2,f3));
