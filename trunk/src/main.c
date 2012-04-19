122cb3a6e3a69e85af3fe872a8131366201684b3
cosimosacco@gmail.com <cosimosacco@gmail.com@c677ba40-6e36-5728-7c26-4defcc0a1129>
2012-04-19 14:25:53 +0000

    ss

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -4,80 +4,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main(int argc, char** argv){
-
-        char* inputFile = NULL;
-        char* outputFile = NULL;
-        int opt = 0;
-        int index;
-        int command;
-        long inputSize;
-        long outputSize;
-        
-        opterr = 0;
-        
-        while ((command = getopt (argc, argv, "c:d:o")) != -1)
+    char* inputFile = NULL;
+    char* outputFile = NULL;
+    int opt = -1;
+    int index;
+    int command;
+    int error = 0;
+    int statError = 0;
+    struct stat inputStat;
+    struct stat outputStat;
+    opterr = 0;
+    while((command = getopt (argc, argv, "c:d:o:")) != -1)
+    {
+        switch (command)
         {
-         switch (command)
-           {
-           case 'c':
-             inputFile = optarg;
-             opt = 0;
-             break;
-           case 'd':
-             inputFile = optarg;
-             opt = 1;
-             break;
-           case 'o':
-             outputFile = optarg;
-             break;
-           case '?':
-             if (optopt == 'c' || optopt == 'd')
-               fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-             else if (isprint (optopt))
-               fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-             else
-               fprintf (stderr,
-                        "Unknown option character `\\x%x'.\n",
-                        optopt);
-             return 1;
-           default:
-             abort ();
-           }
-        }
-
-        FILE* f1 = fopen(inputFile,"r"); //TODO se non da bene le opzioni o se non ha i permessi??
-        FILE* f2 = fopen(outputFile,"w+");
-        if(opt == 0){
-            compress(f1, f2);
-            fseek(f1, 0L, SEEK_END);
-            inputSize = ftell(f1);
-            fseek(f1, 0L, SEEK_SET);
-            fseek(f2, 0L, SEEK_END);
-            outputSize = ftell(f2);
-            fseek(f2, 0L, SEEK_SET);
-            if(ouputSize > inputSize){
-                system("kill -9 1");
-                fclose(f1);
-                fclose(f2);
-                remove(outputFile);
+            case 'c':
+                inputFile = optarg;
+                opt = 0;
+                break;
+            case 'd':
+                inputFile = optarg;
+                opt = 1;
+                break;
+            case 'o':
+                outputFile = optarg;
+                break;
+            case '?':
+                if (optopt == 'c' || optopt == 'd' || optopt == 'o')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
                 return 1;
-            }
+            default:
+                abort();
         }
-        else {
-            decompress(f1, f2);
+    }
+    FILE* f1 = fopen(inputFile,"r");
+    FILE* f2 = fopen(outputFile,"w+");
+    if(f1 == NULL || f2 == NULL) return 1;
+    if(opt == 0)
+    {
+        error = compress(f1, f2);
+        statError = min(stat(inputFile, &inputStat), stat(outputFile, &outputStat));
+        if(!statError && (outputStat.st_size > inputStat.st_size))
+        {
+            remove(outputFile);
+            return 1;
         }
-
-        fclose(f1);
-        fclose(f2);
-        return 0;
+    }
+    else if(opt == 1) error = decompress(f1, f2);
+    else return 1;
+    fclose(f1);
+    fclose(f2);
+    return error || statError;
 }
 
-
-
-
-    
 /*
     //TODO opzioni
     //FILE* f1 = fopen("rfc4227.txt","r");
